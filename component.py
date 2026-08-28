e7058de56f0e65c601de72a48dc01495ffd2ec9a
GoogolFisch <94230796+GoogolFisch@users.noreply.github.com>
2026-08-28 15:08:57 +0200

    structure of the component file

---
+++ component.py
@@ -0,0 +1,42 @@
+
+import random
+import uuid
+
+def getRandName():
+    return f"-{uuid.uuid4()}"
+def getRandNameSmall():
+    return f"-{uuid.uuid4()}"[:9]
+
+class Cost:
+    __slots__ = ("length","errors")
+    def __init__(self,leng,errs):
+        self.length = leng
+        self.errors = errs
+
+    def __str__(self):
+        return f"<{self.length}-{self.errors}>"
+
+    def __eq__(self,other):
+        return self.length == other.length and self.errors == other.errors
+
+    def __lt__(self,other):
+        if(self.errors < other.errors):return True
+        if(self.errors > other.errors):return False
+        if(self.length < other.length):return True
+        return False
+
+    def __gt__(self,other):
+        if(self.errors > other.errors):return True
+        if(self.errors < other.errors):return False
+        if(self.length > other.length):return True
+        return False
+
+    def __add__(self,other):
+        return Cost(self.length + other.length,self.errors + other.errors)
+
+class Wire:pass
+class Via :pass
+class Lane:pass
+class Connection:pass
+
+class Module
